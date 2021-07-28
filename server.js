f54aadc5266d86516381c7dc1e2121d35ee6360f
Rachel Salazar <ret1992@gmail.com>
2021-07-28 11:12:36 -0700

    Successfully implemented authorization of current user using passport

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const profile = require('./routes/api/profile');
 const plants = require('./routes/api/plants');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 // Bringing in body-parser for URL encoding and json
 app.use(bodyParser.urlencoded({extended: false}));
@@ -14,6 +15,12 @@ app.use(bodyParser.json());
 const db = require('./config/keys').mongoURI;
 
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('MongoDB connected')).catch(err => console.log(err));
+
+// Pasport middleware, creating instance of passport
+app.use(passport.initialize());
+
+// Passport configuration
+require('./config/passport')(passport);
 
 app.get('/', (req, res) => res.send('Hello World'));
 
